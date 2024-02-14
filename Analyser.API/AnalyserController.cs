cef7e1622579ec5de3edf17737e86002b2b2b1f2
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-14 14:20:37 +0100

    feat: create controller

---
+++ Analyser.API/AnalyserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Analyser.API.Services;
+using Analyser.API.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Analyser.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyserController : ControllerBase
+    {
+        // POST api/<AnalyserController>
+        [HttpPost]
+        public async Task<IImageData> Post([FromBody] ImageParams data)
+        {
+            GoogleLabelDetectorImpl analyser = new GoogleLabelDetectorImpl();
+            return await analyser.Analyze(data.RemoteFullPath, data.MaxLabels, data.MinConfidenceLevel);
+        }
+    }
+}
