fc2b14a0c94830e8ef8837f80471b9a741335a9f
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-14 14:51:43 +0100

    feat: imported DataObject from BI

---
+++ DataObject.API/Services/IDataObject.cs
@@ -0,0 +1,16 @@
+﻿namespace DataObject.API.Services
+{
+    public interface IDataObject
+    {
+        public Task<bool> DoesExists(string remoteFullPath);
+
+        public Task Upload(string file, string remoteFullPath);
+
+        public Task Download(string remoteFullPath, string localFullPath = "");
+
+        public Task<string> Publish(string remoteFullPath, int expirationTime = 90);
+
+        public Task Remove(string remoteFullPath, bool recursive = false);
+
+    }
+}
