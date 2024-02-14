5445d13f2bce4ee78c0b4c1eafa33cf884d711d8
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-14 14:51:31 +0100

    feat: added exceptions

---
+++ DataObject.API/Exceptions/Exception.cs
@@ -0,0 +1,12 @@
+﻿namespace DataObject.API.Exceptions
+{
+    public class Exception<T>
+    {
+    }
+
+    public class GoogleDataObjectImplException : Exception { }
+
+    public class ObjectAlreadyExistsException : GoogleDataObjectImplException { }
+    public class ObjectNotFoundException : GoogleDataObjectImplException { }
+    public class NotEmptyObjectException : GoogleDataObjectImplException { }
+}
