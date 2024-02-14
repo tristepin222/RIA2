54182ccff8afc1cef250c6f0b3aa407039e0779d
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-14 14:20:06 +0100

    feat: imported Analyser classes from BI

---
+++ Analyser.API/Services/ILabelDetector.cs
@@ -0,0 +1,10 @@
+﻿using Analyser.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analyser.API.Services
+{
+    public interface ILabelDetector
+    {
+        public abstract Task<IImageData> Analyze(string imageString, int labelAmount = 10, int minConfidenceAmount = 90);
+    }
+}
