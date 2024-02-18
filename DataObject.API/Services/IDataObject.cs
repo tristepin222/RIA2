74e627a5e43fdc9d35c9f47cc50531d7fd51ff93
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-18 14:07:13 +0100

    feat: convert to static

--- DataObject.API/Services/IDataObject.cs
+++ DataObject.API/Services/IDataObject.cs
@@ -2,15 +2,15 @@
 {
     public interface IDataObject
     {
-        public Task<bool> DoesExists(string remoteFullPath);
+        public static abstract Task<bool> DoesExists(string remoteFullPath);
 
-        public Task Upload(string file, string remoteFullPath);
+        public static abstract Task Upload(string file, string remoteFullPath);
 
-        public Task Download(string remoteFullPath, string localFullPath = "");
+        public static abstract Task Download(string remoteFullPath, string localFullPath = "");
 
-        public Task<string> Publish(string remoteFullPath, int expirationTime = 90);
+        public static abstract Task<string> Publish(string remoteFullPath, int expirationTime = 90);
 
-        public Task Remove(string remoteFullPath, bool recursive = false);
+        public static abstract Task Remove(string remoteFullPath, bool recursive = false);
 
     }
 }
