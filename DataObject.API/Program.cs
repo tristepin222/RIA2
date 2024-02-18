67fa6dd46c5d5eecfc07040b3e3522f8f037462b
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-18 14:06:55 +0100

    feat: use service

--- DataObject.API/Program.cs
+++ DataObject.API/Program.cs
@@ -1,3 +1,5 @@
+using DataObject.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+GoogleDataObjectImpl.BucketName = Environment.GetEnvironmentVariable("BucketName");
 
 var app = builder.Build();
 
