0e7a9d9ef6b62329ed27b534379448e29d6fc8e0
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-18 15:49:09 +0100

    feat: added base controller

--- Gateway.API/Controllers/GatewayController.cs
+++ Gateway.API/Controllers/GatewayController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace Gateway.API.Controllers
 {
@@ -6,6 +7,39 @@ namespace Gateway.API.Controllers
     [Route("[controller]")]
     public class GatewayController : ControllerBase
     {
-       
+        private HttpClient httpClient;
+        [HttpPost]
+        public async void Call()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.example.com/");
+
+            var task = GoogleDataObjectImpl.DoesExists(imageName);
+            task.Wait();
+            if (!task.Result)
+            {
+                var taskUpload = GoogleDataObjectImpl.Upload(imageName, imageName);
+                taskUpload.Wait();
+            }
+        }
+        [HttpPost]
+        public async Task<string> Publish(string remoteFullPath, int expirationTime = 90)
+        {
+            return await GoogleDataObjectImpl.Publish(remoteFullPath, expirationTime);
+        }
+        [HttpPost]
+        public async Task<JsonResult> Analyze(string remoteFullPath, int maxLabels = 10, int minConfidenceLevel = 90)
+        {
+            GoogleLabelDetectorImpl analyser = new GoogleLabelDetectorImpl();
+
+            IImageData img = await analyser.Analyze(remoteFullPath, maxLabels, minConfidenceLevel);
+            SQLDumber.Dumb(img);
+            return img;
+        }
+        [HttpPost]
+        public async void Upload(string localFullPath, string remoteFullPath)
+        {
+            await GoogleDataObjectImpl.Upload(localFullPath, remoteFullPath);
+        }
     }
 }
