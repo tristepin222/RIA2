ba7bd7289987e9a56bcebbaef708d64f3529c124
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-18 14:05:03 +0100

    feat: added DataObjectParam

---
+++ DataObject.API/Models/DataObjectParam.cs
@@ -0,0 +1,9 @@
+﻿namespace DataObject.API.Models
+{
+    public class DataObjectParam
+    {
+        public string LocalFullPath { get; set; }
+        public string RemoteFullPath { get; set; }
+        public int ExpirationTime { get; set; }
+    }
+}
