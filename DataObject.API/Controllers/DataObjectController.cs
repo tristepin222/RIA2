ee3096cea25060975cf12c1c9c09dd593cf7ce35
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-02-18 13:42:41 +0100

    feat: added controller

---
+++ DataObject.API/Controllers/DataObjectController.cs
@@ -0,0 +1,28 @@
+﻿using DataObject.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DataObject.API.Controllers
+{
+    public class DataObjectController
+    {
+        [ApiController]
+        [Route("api/v1/[controller]")]
+        public class ShoppingController : ControllerBase
+        {
+
+            [HttpPost]
+            public async void Upload([FromBody] ImageParams data)
+            {
+                await GoogleDataObjectImpl.Upload(data.localFullPath, data.remoteFullPath);
+            }
+
+            [HttpPost]
+            public async Task<string> Publish([FromBody] ImageParams data)
+            {
+                return await GoogleDataObjectImpl.Publish(data.remoteFullPath, data.expirationTime);
+            }
+
+        }
+    }
+}
